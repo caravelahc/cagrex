ec75e55fc5f633a13e156162709c526b295c1ac0
Cauê Baasch de Souza <cauebs@protonmail.com>
2018-02-26 21:16:51 -0300

    Forgot to remove grequests from requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cagrex',
-    version='0.4.2',
+    version='0.4.3',
     packages=find_packages(),
-    install_requires=['requests', 'grequests', 'bs4', 'mechanicalsoup'],
+    install_requires=['requests', 'bs4', 'mechanicalsoup'],
 )
