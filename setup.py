2ec11c448c3cefba96e15f492e15ccf83f494ec0
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2019-10-09 19:42:05 -0300

    Update version and remove setup.py.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-
-setup(
-    name='cagrex',
-    version='0.4.3',
-    packages=find_packages(),
-    install_requires=['requests', 'bs4', 'mechanicalsoup'],
-)
